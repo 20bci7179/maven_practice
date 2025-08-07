57aedff3907a0bc2fae53bd12ee96a42c7a8f99a
20bci7179 <126901595+20bci7179@users.noreply.github.com>
2025-08-07 22:07:39 +0530

    Update JenkinsCal.java

--- src/main/java/com/gopi/training/jenkins/JenkinsCal.java
+++ src/main/java/com/gopi/training/jenkins/JenkinsCal.java
@@ -2,13 +2,24 @@ package com.gopi.training.jenkins;
 
 public class JenkinsCal {
 	
-	//addition of 2 numbers
-	public int addNumbers(int num1,int num2) {
-		return num1+num2;
+	// addition of 2 numbers
+	public int addNumbers(int num1, int num2) {
+		return num1 + num2;
 	}
 	
-	//subtraction of 2 numbers
+	// subtraction of 2 numbers
 	public int subtractNumbers(int num1, int num2) {
-		return num1-num2;
+		return num1 - num2;
+	}
+
+	// main method to print outputs
+	public static void main(String[] args) {
+		JenkinsCal calc = new JenkinsCal();
+
+		int addResult = calc.addNumbers(10, 5);
+		int subResult = calc.subtractNumbers(10, 5);
+
+		System.out.println("Addition: " + addResult);
+		System.out.println("Subtraction: " + subResult);
 	}
 }
